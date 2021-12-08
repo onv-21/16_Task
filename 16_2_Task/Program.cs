9fcc0eef6fdf5ea9e24ebbc1181fdb6869ddb3ba
Администратор <@DESKTOP-PJPICF5>
2021-12-08 16:31:07 +0800

    Добавьте файлы проекта.

---
+++ 16_2_Task/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+using Task16;
+
+StreamReader streamReader = new StreamReader("C:\\Users\\Администратор\\OneDrive\\Autodesk_ITMO_2021\\1_CSharp_Base\\N_Task\\16_Task\\16_Task\\bin\\Debug\\net6.0\\Products.json");
+string json = streamReader.ReadLine();
+Products[] products = JsonSerializer.Deserialize<Products[]>(json);
+
+Products productMax = products[0];
+for (int i=0; i<5; i++)
+{
+    if (products[i].Price > productMax.Price)
+    {
+        productMax = products[i];
+    }   
+}
+Console.WriteLine(productMax.Name);
+
---
+++ 16_Task/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Text.Json;
+
+
+
+Products[] products = new Products[5];
+for (int i = 0; i < 5; i++)
+{
+    products[i] = new Products();
+    Console.WriteLine("Введите код товара:");
+    products[i].Code = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите наименование товара:");
+    products[i].Name = Console.ReadLine();
+    Console.WriteLine("Введите цену товара:");
+    products[i].Price = Convert.ToInt32(Console.ReadLine());
+}
+
+string json= JsonSerializer.Serialize<Products[]>(products);
+Console.WriteLine(json);
+string path = "Products.json";
+if (!File.Exists(path))
+{
+    File.Create(path);
+}
+StreamWriter sw = new StreamWriter(path);
+{
+    sw.WriteLine(json);
+    sw.Close();
+}
+class Products
+{
+    
+    public int Code { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    
+}
+
+
+
